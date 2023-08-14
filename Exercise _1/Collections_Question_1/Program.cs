1862eed0a0b76d5821fdb506dae4781d8697bcd9
Yiğit Çizmeci <yigitcizmeci@hotmail.com>
2023-08-14 17:23:09 +0300

    Questions

---
+++ Exercise _1/Collections_Question_1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+          Enter 20 positive numbers from the keyboard and separate them into two different lists: prime and non-prime. (Use the ArrayList class.)
+
+        Prevent negative and non-numeric inputs.
+        Display the elements of each list in descending order.
+        Display the count and average of elements in both lists on the screen.
+         */
+
+        ArrayList list1 = new ArrayList();
+        ArrayList list2 = new ArrayList();
+        double avarage = 0;
+        double avarage2 = 0;
+
+        Console.WriteLine("Please enter 20 positive number");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write((i + 1) + ". number : ");
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+
+                if (int.TryParse(input, out int number) && number > 0)
+                {
+                    bool IsPrime = true;
+
+                    for (int k = 2; k < number - 1; k++)
+                    {
+                        if (number % k == 0)
+                        {
+                            list2.Add(number);
+                            avarage2 += number;
+                            IsPrime = false;
+                            break;
+                        }
+
+                    }
+                    if (IsPrime)
+                    {
+                        list1.Add(number);
+                        avarage += number;
+                    }
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a positive number.");
+                }
+            }
+        }
+        avarage2 = avarage2 / list2.Count;
+        avarage = avarage / list1.Count;
+
+        list1.Sort();
+        list2.Sort();
+        list1.Reverse();
+        list2.Reverse();
+
+        Console.WriteLine("***** Prime Numbers *****");
+        foreach (int item in list1)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine("================== Total : " + list1.Count + " numbers " + "=====" + " Avarage : " + avarage + " ==================");
+        Console.WriteLine("***** NonPrime Numbers *****");
+        foreach (int item in list2)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine("================== Total : " + list2.Count + " numbers " + "=====" + " Avarage : " + avarage2 + " ==================");
+
+        Console.ReadLine();
+    }
+
+
+
+
+}
---
+++ Exercise _1/Collections_Question_2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+
+internal class Program
+{
+    /*
+     Klavyeden girilen 20 adet sayının en büyük 3 tanesi ve en küçük 3 tanesi bulan,
+    her iki grubun kendi içerisinde ortalamalarını alan ve bu ortalamaları ve 
+    ortalama toplamlarını console'a yazdıran programı yazınız. (Array sınıfını kullanarak yazınız.)
+    */
+    private static void Main(string[] args)
+    {
+        ArrayList arrayList = new ArrayList();
+
+        Console.WriteLine("Please enter 20 number : ");
+        for (int i = 0; i < 20; i++)
+        {
+            Console.Write((i + 1) + ". Number : ");
+            while (true)
+            {
+                String numbers = Console.ReadLine();
+
+                if (int.TryParse(numbers, out int number) && number > 0)
+                {
+                    arrayList.Add(number);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a number only.");
+                }
+            }
+
+        }
+        double MinAvarage = 0;
+        double MaxAvarage = 0;
+
+        arrayList.Sort();
+        ArrayList MinFirstThree = arrayList.GetRange(0, 3);
+        Console.WriteLine("******* Minumum 3 Numbers ******");
+        foreach (int i in MinFirstThree)
+        {
+            Console.WriteLine(i);
+            MinAvarage += i;
+        }
+        MinAvarage = MinAvarage / MinFirstThree.Count;
+
+        Console.WriteLine("===== Avarage : " + MinAvarage + " =====");
+        arrayList.Reverse();
+        ArrayList MaxFirstThree = arrayList.GetRange(0, 3);
+        Console.WriteLine("******* Maximum 3 Numbers ******");
+        foreach (int i in MaxFirstThree)
+        {
+            Console.WriteLine(i);
+            MaxAvarage += i;
+        }
+        MaxAvarage = MaxAvarage / MaxFirstThree.Count;
+
+        Console.WriteLine("===== Avarage : " + MaxAvarage + " =====");
+        Console.WriteLine("=========== Sum of avarages : " + (MinAvarage + MaxAvarage) + " ===========");
+        Console.ReadLine();
+
+    }
+}
